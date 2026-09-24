a912f4e57253707b70652a275e82752c04bf8efa
Isaac Cheng <xianbing@stanford.edu>
2026-09-23 23:15:45 -0700

    vanilla

--- src/agents/pg_agent.py
+++ src/agents/pg_agent.py
@@ -79,8 +79,8 @@ class PGAgent(nn.Module):
         )
 
         # step 3: use all datapoints (s_t, a_t, adv_t) to update the PG actor/policy
-        # TODO: update the PG actor/policy network once using the advantages
-        info: dict = None
+        # update the PG actor/policy network once using the advantages
+        info: dict = self.actor.update(obs, actions, advantages)
 
         # step 4: if needed, use all datapoints (s_t, a_t, q_t) to update the PG critic/baseline
         if self.critic is not None:
